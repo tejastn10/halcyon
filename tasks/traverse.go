4978b906e5d780cddf9a858c9ace6b01780d7ea1
Tejas Nikhar <tejastn10@gmail.com>
2024-12-09 11:16:47 +0530

    feat: add basic traverse package to print filepath and size

---
+++ tasks/traverse.go
@@ -0,0 +1,47 @@
+package tasks
+
+import (
+	"io/fs"
+	"path/filepath"
+)
+
+// FileInfo holds the basic details of a file
+type FileInfo struct {
+	Path string
+	Size int64
+}
+
+// TraverseDirectory traverses a directory recursively and returns information about all files.
+func TraverseDirectory(dir string) ([]FileInfo, error) {
+	var files []FileInfo
+
+	// Walk through the directory
+	err := filepath.WalkDir(dir, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			// Return error if any issues occur during traversal
+			return err
+		}
+
+		// Only process files, not directories
+		if !d.IsDir() {
+			info, err := d.Info()
+
+			if err != nil {
+				return err
+			}
+
+			files = append(files, FileInfo{
+				Path: path,
+				Size: info.Size(),
+			})
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return files, nil
+}
