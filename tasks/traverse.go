84ddc8eb53942f464bbd710467e90ad0ccf6c0ff
Tejas Nikhar <tejastn10@gmail.com>
2024-12-10 06:33:27 +0530

    feat: update FileInfo to have canonical names of files

--- tasks/traverse.go
+++ tasks/traverse.go
@@ -3,17 +3,21 @@ package tasks
 import (
 	"io/fs"
 	"path/filepath"
+	"strconv"
+
+	"github.com/tejastn10/halcyon/utils"
 )
 
 // FileInfo holds the basic details of a file
 type FileInfo struct {
-	Path string
-	Info fs.FileInfo
+	Path      string
+	Canonical string
+	Info      fs.FileInfo
 }
 
 // TraverseDirectory traverses a directory recursively and returns information about all files.
-func TraverseDirectory(dir string) ([]FileInfo, error) {
-	var files []FileInfo
+func TraverseDirectory(dir string) (map[string][]FileInfo, error) {
+	fileMap := make(map[string][]FileInfo)
 
 	// Walk through the directory
 	err := filepath.WalkDir(dir, func(path string, d fs.DirEntry, err error) error {
@@ -30,9 +34,13 @@ func TraverseDirectory(dir string) ([]FileInfo, error) {
 				return err
 			}
 
-			files = append(files, FileInfo{
-				Path: path,
-				Info: info,
+			canonical := utils.GetCanonicalName(info.Name())
+			key := canonical + "_" + strconv.FormatInt(info.Size(), 10) // Group by name and size
+
+			fileMap[key] = append(fileMap[key], FileInfo{
+				Path:      path,
+				Canonical: canonical,
+				Info:      info,
 			})
 		}
 
@@ -43,5 +51,5 @@ func TraverseDirectory(dir string) ([]FileInfo, error) {
 		return nil, err
 	}
 
-	return files, nil
+	return fileMap, nil
 }
