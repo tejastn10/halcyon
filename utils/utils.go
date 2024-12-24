5ea20740fc1d18b155f56cd4fe765ba0d3fcc8f8
Tejas Nikhar <tejastn10@gmail.com>
2024-12-24 19:30:25 +0530

    feat: enhance GetCanonicalName to handle more duplicate patterns

--- utils/utils.go
+++ utils/utils.go
@@ -1,14 +1,34 @@
 package utils
 
 import (
+	"path/filepath"
 	"regexp"
 	"strings"
 )
 
-// GetCanonicalName removes common suffixes like "_copy", "(1)", etc.
-func GetCanonicalName(name string) string {
-	// Remove common patterns for duplicates
-	re := regexp.MustCompile(`(?i)(\s*$begin:math:text$copy|\\s*\\(\\d+$end:math:text$|_copy))$`)
-	cleanName := re.ReplaceAllString(name, "")
-	return strings.TrimSpace(cleanName)
+var (
+	duplicatePatterns = []*regexp.Regexp{
+		regexp.MustCompile(`(?i)(_copy|[\s-]copy)`), // matches _copy, -copy, space copy (case insensitive)
+		regexp.MustCompile(`\s*\(\d+\)$`),           // matches (1), (2)
+		regexp.MustCompile(`\s*_\d+$`),              // matches _1, _2
+		regexp.MustCompile(`\s*-\d+$`),              // matches -1, -2
+		regexp.MustCompile(`\s+\d+$`),               // matches " 1", " 2"
+	}
+)
+
+func GetCanonicalName(filePath string) string {
+	baseName := filepath.Base(filePath)
+
+	ext := filepath.Ext(baseName)
+	nameWithoutExt := strings.TrimSuffix(baseName, ext)
+
+	// Apply patterns
+	for _, pattern := range duplicatePatterns {
+		nameWithoutExt = pattern.ReplaceAllString(nameWithoutExt, "")
+	}
+
+	// Clean up any remaining whitespace
+	nameWithoutExt = strings.TrimSpace(nameWithoutExt)
+
+	return strings.ToLower(nameWithoutExt) + strings.ToLower(ext)
 }
